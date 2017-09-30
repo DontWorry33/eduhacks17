d7bfda4d6c73aa0476782e171b77a1da7a45c33f
Afke Holgersen <aholgers@sfu.ca>
2017-09-30 16:35:01 -0700

    create room and room stats pages added, splash shell created

--- static/splash.js
+++ static/splash.js
@@ -1,3 +1,15 @@
+var username= new Vue({
+    el: '#user-name',
+    data: {
+        message: 'Username:'
+    }
+})
+var roomname = new Vue({
+    el: '#room-name',
+    data: {
+        message: 'Room Name:'
+    }
+})
 var app = new Vue({
     el: '#app',
     data: {
