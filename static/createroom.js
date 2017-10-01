4e5809a8132c8f1e299c9b6d687f2943fa52d996
Jason <jason741852@gmail.com>
2017-09-30 21:01:38 -0700

    Add create room method

--- static/createroom.js
+++ static/createroom.js
@@ -1,6 +1,39 @@
-var main = new Vue({
-    el: '#main',
-    data: {
-        message: 'create room'
+$(document).ready(function() {
+    // Use a "/test" namespace.
+    // An application can open a connection on multiple namespaces, and
+    // Socket.IO will multiplex all those connections on a single
+    // physical channel. If you don't care about multiple channels, you
+    // can set the namespace to an empty string.
+    namespace = '/test';
+
+    // Connect to the Socket.IO server.
+    // The connection URL has the following format:
+    //     http[s]://<domain>:<port>[/<namespace>]
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
+    console.log(location.protocol + '//' + document.domain + ':' + location.port + namespace)
+
+    // Event handler for new connections.
+    // The callback function is invoked when a connection with the
+    // server is established.
+    socket.on('connect', function() {
+        socket.emit('my_event', {data: 'I\'m connected!'});
+    });
+
+    socket.on('redirect', function (data) {
+        window.location = data.url;
+    });
+
+    // Event handler for server sent data.
+    // The callback function is invoked whenever the server emits data
+    // to the client. The data is then displayed in the "Received"
+    // section of the page.
+    socket.on('my_response', function(msg) {
+        $('#log').append('<br>' + $('<div/>').text('Received #' + msg.count + ': ' + msg.data).html());
+    });
+
+
+    $('form#create').submit(function(event) {
+      socket.emit('create_room', {room: $('#room_name').val(), title: $('#room_title').val(), questions: $('#question').val(), solutions: $('#output').val()});
+      return false;
     }
-})
+}
